7822d0d4a621370b85e43dc6668eb639abbc89bb
Séra Bálint <sera.balint@e-vista.hu>
2016-06-12 11:23:06 +0200

    wp plugin file added

---
+++ wp-comp-plug-pack.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @package evista/wp-comp-plug-pack
+ * @version 1.0
+ */
+/*
+Plugin Name: Evista Wordpress Plugin
+Plugin URI: http://evista-agency/wp-comp-plug-pack
+Description: This is a dedicated plugin.
+Author: Balint Sera
+Version: 1.0
+*/
